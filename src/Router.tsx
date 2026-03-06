ba8edb050d1307b22c9a33bb5beff8b68c1766f5
Andreas Ögren <andreas.ogren@gmail.com>
2026-03-06 15:42:05 +0100

    Support subpaths

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes, useLocation } from "react-router-dom";
 import Index from "./pages/Index";
 import { langLoader } from "./pages/langLoader";
 import NotFound from "./pages/NotFound";
@@ -8,14 +8,25 @@ function LanguageRedirect() {
 	return <Navigate to={`/${lang}`} replace />;
 }
 
+function LangGuard({ children }: { children: React.ReactNode }) {
+	const location = useLocation();
+	const lang = location.pathname.split("/")[1];
+	if (lang !== "sv" && lang !== "en") {
+		return <Navigate to="/en" replace />;
+	}
+	return <>{children}</>;
+}
+
 export default function AppRouter() {
 	return (
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<LanguageRedirect />} />
-				<Route path=":lang" element={<Index />} loader={langLoader} />
-				<Route path="*" element={<NotFound />} />
-			</Routes>
+			<LangGuard>
+				<Routes>
+					<Route path="/" element={<LanguageRedirect />} />
+					<Route path=":lang" element={<Index />} loader={langLoader} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</LangGuard>
 		</BrowserRouter>
 	);
 }
